e8060e54fc6a6dd96fbe0e63bb1ed1344be35496
Andrew Charneski <acharneski@gmail.com>
2025-05-13 22:48:55 -0400

    Update EncryptionUtilTest.kt

--- intellij/src/test/kotlin/com/simiacryptus/util/EncryptionUtilTest.kt
+++ intellij/src/test/kotlin/com/simiacryptus/util/EncryptionUtilTest.kt
@@ -3,7 +3,9 @@ package com.simiacryptus.util
 import com.simiacryptus.cognotik.util.EncryptionUtil
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 object EncryptionUtilTest {
 
     @Test
