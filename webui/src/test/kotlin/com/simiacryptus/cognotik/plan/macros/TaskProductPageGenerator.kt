7b85baa2b808c3bf65dacc0f1a15555133851611
Andrew Charneski <acharneski@gmail.com>
2026-01-13 19:28:09 -0500

    wip

--- webui/src/test/kotlin/com/simiacryptus/cognotik/plan/macros/TaskProductPageGenerator.kt
+++ webui/src/test/kotlin/com/simiacryptus/cognotik/plan/macros/TaskProductPageGenerator.kt
@@ -2,7 +2,6 @@ package com.simiacryptus.cognotik.plan.macros
 
 import com.simiacryptus.cognotik.plan.tools.file.ReadDocumentsTask.Companion.getAvailableFiles
 import java.io.File
-import kotlin.io.path.Path
 
 object TaskProductPageGenerator : com.simiacryptus.cognotik.util.FileGenerator() {
   @JvmStatic fun main(args: Array<String>) {
@@ -16,13 +15,16 @@ object TaskProductPageGenerator : com.simiacryptus.cognotik.util.FileGenerator()
         listOf(
           it.toString(),
           it.toString().replace("/main/", "/test/"),
+          "docs/embedding.md",
           "docs/task_product_page.md",
           "site/cognotik.com/task_product_page_template.html",
         )
       },
       generationPrompt = { source, target ->
-        "Update the product page HTML file ($target) to reflect the latest implementation in ${source.name}." +
-            "In particular, add the browser feature for the test workspace: \n" + getAvailableFiles(File("webui/workspaces/${source.nameWithoutExtension}").toPath()).joinToString("\n") { "      - $it" }
+        "Update the product page HTML file ($target) to reflect the latest implementation in ${source.name}.\n" +
+            "In particular: \n" +
+            "* add the browser feature for the test workspace: \n" + getAvailableFiles(File("webui/workspaces/${source.nameWithoutExtension}").toPath()).joinToString("\n") { "      - $it" } + "\n" +
+            "* Add/update details on how to invoke using the embedding doc and test case example.\n"
       }
     )
   }
