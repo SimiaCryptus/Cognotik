1097dcd5cf946acd639a6fa77dca951d13b27ed6
Andrew Charneski <acharneski@gmail.com>
2026-03-17 19:42:18 -0400

    fix

--- core/src/main/kotlin/com/simiacryptus/cognotik/diff/DiffUtil.kt
+++ core/src/main/kotlin/com/simiacryptus/cognotik/diff/DiffUtil.kt
@@ -16,9 +16,7 @@ data class PatchLine(
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-
         other as PatchLine
-
         return compareText == other.compareText
     }
 
